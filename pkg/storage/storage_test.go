f6dae03c1bd159330b38562521c3498449c1b00d
Anton Kilchik <anton.kilchik@gmail.com>
2019-10-31 17:29:08 +0300

    add some tests

--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -1,1 +1,125 @@
 package storage
+
+import (
+	"boats/clients/nausys"
+	"context"
+	_ "github.com/jackc/pgx/stdlib"
+	"github.com/jmoiron/sqlx"
+	"github.com/pressly/goose"
+	"github.com/stretchr/testify/suite"
+	"os"
+	"testing"
+)
+
+
+type TestStorageSuite struct {
+	suite.Suite
+
+	db *sqlx.DB
+	store  *StorageImpl
+}
+
+func (s *TestStorageSuite) SetupTest() {
+	s.db = s.initDB()
+	s.store = NewStorageImpl(s.db)
+}
+
+func (s *TestStorageSuite) initDB() *sqlx.DB {
+	dsn := os.Getenv("DSN")
+	dsn = "postgres://testusr:123@127.0.0.1:5432/boats_db"
+	if dsn == "" {
+		s.FailNow("pass DSN in env var")
+	}
+
+	sqlDB := sqlx.MustConnect("pgx", dsn)
+
+	err := goose.Up(sqlDB.DB, "../../migrations")
+	if err != nil {
+		s.FailNow("migrate", err)
+	}
+
+	return sqlDB
+}
+
+func (s *TestStorageSuite) TearDownTest() {
+	s.store.ClearAll(context.Background(), s.db)
+	s.db.Close()
+}
+
+func (s *TestStorageSuite) TestStorageImpl_FindYachts() {
+	ctx := context.Background()
+	s.store.InsertBuilders(ctx, s.db, []nausys.Builder{
+		{
+			Id:1,
+			Name:"builder abc",
+		},
+		{
+			Id:2,
+			Name:"builder def",
+		},
+	})
+	s.store.InsertModels(ctx, s.db, []nausys.Model{
+		{
+			Id:1,
+			Name:"model ghi",
+			BuilderId:2,
+		},
+		{
+			Id:2,
+			Name:"model jkl",
+			BuilderId:1,
+		},
+	})
+	s.store.InsertCharters(ctx, s.db, []nausys.Charter{
+		{
+			Id:1,
+			Name:"charter mno",
+		},
+		{
+			Id:2,
+			Name:"charter pqr",
+		},
+	})
+	s.store.InsertYachts(ctx, s.db, []*nausys.Yacht{
+		{
+			Id:1,
+			Name:"yacht1",
+			CharterId:2,
+			ModelId:1,
+		},
+		{
+			Id:2,
+			Name:"yacht2",
+			CharterId:2,
+			ModelId:1,
+		},
+		{
+			Id:3,
+			Name:"yacht3",
+			CharterId:1,
+			ModelId:2,
+		},
+	})
+
+	for _, tcase := range []struct{
+		builderPrefix string
+		modelPrefix string
+		expectedYachtName string
+	} {
+		{
+			builderPrefix:"builder ab",
+			modelPrefix:"model jk",
+			expectedYachtName:"yacht3",
+		},
+	} {
+		yachts, total, err := s.store.FindYachts(ctx, tcase.builderPrefix, tcase.modelPrefix, 100, 0)
+		s.NoError(err)
+		s.Equal(int64(1), total)
+		s.Equal(1, len(yachts))
+		s.Equal(tcase.expectedYachtName, yachts[0].Name)
+	}
+}
+
+func TestStorageTestSuite(t *testing.T) {
+	suite.Run(t, new(TestStorageSuite))
+}
--- pkg/syncer/syncer_test.go
+++ pkg/syncer/syncer_test.go
@@ -1,1 +1,80 @@
 package syncer
+
+import (
+	"database/sql"
+	assert2 "github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestFindNextFreeSlot(t *testing.T) {
+	assert := assert2.New(t)
+
+	date := func(str string) time.Time {
+		d, _ := time.Parse("02.01.2006", str)
+		return d
+	}
+
+	for _, tcase := range []struct{
+		reserved *reservSlots
+		expectedFrom sql.NullTime
+		expectedTo sql.NullTime
+	} {
+		{
+			reserved:&reservSlots{
+				from:[]time.Time{
+					date("29.06.2019"),
+				},
+				to:[]time.Time{
+					date("29.07.2019"),
+				},
+			},
+			expectedFrom:sql.NullTime{},
+			expectedTo:sql.NullTime{},
+		},
+		{
+			reserved:&reservSlots{
+				from:[]time.Time{
+					date("29.10.2019"),
+				},
+				to:[]time.Time{
+					date("27.11.2019"),
+				},
+			},
+			expectedFrom:sql.NullTime{date("28.11.2019"), true},
+			expectedTo:sql.NullTime{},
+		},
+		{
+			reserved:&reservSlots{
+				from:[]time.Time{
+					date("15.11.2019"),
+				},
+				to:[]time.Time{
+					date("15.12.2019"),
+				},
+			},
+			expectedFrom:sql.NullTime{date("30.10.2019"),true},
+			expectedTo:sql.NullTime{date("14.11.2019"), true},
+		},
+		{
+			reserved:&reservSlots{
+				from:[]time.Time{
+					date("15.10.2019"),
+					date("16.12.2019"),
+					date("02.03.2020"),
+				},
+				to:[]time.Time{
+					date("15.12.2019"),
+					date("16.01.2020"),
+					date("02.04.2020"),
+				},
+			},
+			expectedFrom:sql.NullTime{date("17.01.2020"), true},
+			expectedTo:sql.NullTime{date("01.03.2020"), true},
+		},
+	} {
+		from, to := findNextFreeSlot(date("30.10.2019"), tcase.reserved)
+		assert.Equal(tcase.expectedFrom, from)
+		assert.Equal(tcase.expectedTo, to)
+	}
+}
